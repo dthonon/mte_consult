dc29ae1672d9e562e8ba58462c3b32458bd253d0
Daniel Thonon <d.thonon9@gmail.com>
2022-02-10 13:50:00 +0100

    Améliorations Flake8

--- scrapper/mte/middlewares.py
+++ scrapper/mte/middlewares.py
@@ -2,11 +2,10 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
+from itemadapter import is_item
+from itemadapter import ItemAdapter
 from scrapy import signals
-
 # useful for handling different item types with a single interface
-from itemadapter import is_item, ItemAdapter
 
 
 class MteSpiderMiddleware:
@@ -33,8 +32,7 @@ class MteSpiderMiddleware:
         # it has processed the response.
 
         # Must return an iterable of Request, or item objects.
-        for i in result:
-            yield i
+        yield from result
 
     def process_spider_exception(self, response, exception, spider):
         # Called when a spider or process_spider_input() method
@@ -49,11 +47,10 @@ class MteSpiderMiddleware:
         # that it doesn’t have a response associated.
 
         # Must return only requests (not items).
-        for r in start_requests:
-            yield r
+        yield from start_requests
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class MteDownloaderMiddleware:
@@ -100,4 +97,4 @@ class MteDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
