de09cfbac668b1ae43045237fab962593342732e
Daniel Thonon <d.thonon9@gmail.com>
2023-06-23 13:16:19 +0200

    Analyse consultation ESOD Fixes #208 - WIP

--- src/mte_consult/__main__.py
+++ src/mte_consult/__main__.py
@@ -3,53 +3,58 @@ import csv
 import hashlib
 import logging
 import re
-import unicodedata
+# import unicodedata
 from pathlib import Path
 from typing import Any
+from functools import partial
 
 import click
 import hunspell
 import pandas as pd
+import spacy
 import textacy
 from textacy import preprocessing
 
+
+# Spell checking word counter (global)
+nb_words = 0
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 
-def _clean_unicode(self, ch):
-    """Remove unprintable character
-    You can find a full list of categories here:
-    http://www.fileformat.info/info/unicode/category/index.htm"""
-    letters = ("LC", "Ll", "Lm", "Lo", "Lt", "Lu")
-    numbers = ("Nd", "Nl", "No")
-    marks = ("Mc", "Me", "Mn")
-    punctuation = ("Pc", "Pd", "Pe", "Pf", "Pi", "Po", "Ps")
-    symbol = ("Sc", "Sk", "Sm", "So")
-    space = ("Zl", "Zp", "Zs")
+# def _clean_unicode(self, ch):
+#     """Remove unprintable character
+#     You can find a full list of categories here:
+#     http://www.fileformat.info/info/unicode/category/index.htm"""
+#     letters = ("LC", "Ll", "Lm", "Lo", "Lt", "Lu")
+#     numbers = ("Nd", "Nl", "No")
+#     marks = ("Mc", "Me", "Mn")
+#     punctuation = ("Pc", "Pd", "Pe", "Pf", "Pi", "Po", "Ps")
+#     symbol = ("Sc", "Sk", "Sm", "So")
+#     space = ("Zl", "Zp", "Zs")
 
-    allowed_categories = letters + numbers + marks + punctuation + symbol
-    if unicodedata.category(ch) in space:
-        return " "
-    elif unicodedata.category(ch) in allowed_categories:
-        return ch
-    else:
-        return " "
+#     allowed_categories = letters + numbers + marks + punctuation + symbol
+#     if unicodedata.category(ch) in space:
+#         return " "
+#     elif unicodedata.category(ch) in allowed_categories:
+#         return ch
+#     else:
+#         return " "
 
 
-def _remove_tags(self, text):
-    """Cleans a string :
-    - removing HTML tags
-    - removing shortcuts, such as =>...
-    - removing non printable text, based on a whitelist of printable unicode
-    """
-    TAG_RE = re.compile(r"<[^>]+>|[=\-,]?>|\+|\/\/")
-    text = TAG_RE.sub("", text)
+# def _remove_tags(self, text):
+#     """Cleans a string :
+#     - removing HTML tags
+#     - removing shortcuts, such as =>...
+#     - removing non printable text, based on a whitelist of printable unicode
+#     """
+#     TAG_RE = re.compile(r"<[^>]+>|[=\-,]?>|\+|\/\/")
+#     text = TAG_RE.sub("", text)
 
-    text = "".join([self._clean_unicode(c) for c in text])
-    return text
+#     text = "".join([self._clean_unicode(c) for c in text])
+#     return text
 
 
 @click.version_option()
@@ -83,8 +88,8 @@ def main(
 
     ctx.obj["CONSULTATION"] = consultation
     ctx.obj["DATA_DIRECTORY"] = data_directory
-    ctx.obj["START_COMMENT"] = start_comment
-    ctx.obj["END_COMMENT"] = end_comment
+    ctx.obj["START_COMMENT"] = start_comment if start_comment>1 else None
+    ctx.obj["END_COMMENT"] = end_comment if end_comment>0 else None
 
 
 @main.command()
@@ -126,8 +131,9 @@ def preprocess(ctx: click.Context) -> None:
     preproc = preprocessing.make_pipeline(
         preprocessing.normalize.whitespace,
         preprocessing.replace.urls,
-        preprocessing.replace.numbers,
+        partial(preprocessing.replace.numbers, repl="NOMBRE"),
         preprocessing.replace.emojis,
+       preprocessing.replace.emails,
         preprocessing.replace.currency_symbols,
         preprocessing.remove.html_tags,
     )
@@ -164,7 +170,8 @@ def _spell_correction(doc: str, spell: Any) -> str:
                 text += d.text_with_ws
     return text
 
-def _fr_nlp(self):
+
+def _fr_nlp() -> spacy.Language:
     # Prepare NLP processing
     logging.info(f"Préparation du traitement NLP")
     # spacy.prefer_gpu()
@@ -177,46 +184,48 @@ def _fr_nlp(self):
     _nlp.Defaults.stop_words -= {"contre"}
     return _nlp
 
+
 @main.command()
 @click.pass_context
-def process(ctx: click.Context) -> None:
-    """Traitement principal des données."""
+def check(ctx: click.Context) -> None:
+    """Correction orthographique des commentaires."""
     consultation = ctx.obj["CONSULTATION"]
     data_dir = ctx.obj["DATA_DIRECTORY"]
-    logging.info(f"Traitement principal de {consultation} dans {data_dir}")
+    start_comment = ctx.obj["START_COMMENT"]
+    end_comment = ctx.obj["END_COMMENT"]
+    logging.info(f"Correction orthographique des commentaires de {consultation}")
     csv_file = Path(data_dir + "/preprocessed/" + consultation + ".csv")
-    logging.debug("Lecture %s", csv_file)
-    responses = pd.read_csv(csv_file, header=0, quoting=csv.QUOTE_ALL, nrows=1000000)
+    logging.debug(f"Lecture {csv_file} depuis {start_comment} jusqu'à {end_comment}")
+    responses = pd.read_csv(csv_file, header=0, quoting=csv.QUOTE_ALL, nrows=end_comment)
     logging.info("Nombre de commentaires à traiter : %d", len(responses))
 
-    # Read classification data, if it exists
-    csv_file = Path(data_dir + "/external/" + consultation + ".csv")
-    if csv_file.exists():
-        logging.info(f"Chargement des classifications {csv_file}")
-        classif = pd.read_csv(csv_file, header=0, quoting=csv.QUOTE_ALL, nrows=1000000)
-        logging.info(f"Lu {len(classif)} données de classification")
-        classif = classif.drop(columns=["raw_text"])
-        responses = responses.merge(classif, on="uid")
-    else:
-        logging.info(f"Pas de classification found dans {csv_file}")
-        classif = None
-
     # Correction orthographique
-    logging.info(f"Correction orthographique des commentaires")
     spell = hunspell.HunSpell(
         "/usr/share/hunspell/fr_FR.dic", "/usr/share/hunspell/fr_FR.aff"
     )
     added_words = Path(data_dir + "/external/" + "mtes.txt")
     spell.add_dic(added_words)
     # spell.remove("abatage")
+    tokenizer = _fr_nlp().tokenizer
     responses["checked_text"] = responses["raw_text"].apply(
-        lambda d: _spell_correction(_fr_nlp(d), spell))
+        lambda d: _spell_correction(tokenizer(d), spell)
+    )
     # Save processed data to csv file
     csv_file = Path(data_dir + "/interim/" + consultation + ".csv")
-    logging.info(
-        f"Storing {len(responses)} rows of processed data to {csv_file}"
-    )
+    logging.info(f"Storing {len(responses)} rows of processed data to {csv_file}")
     responses.to_csv(csv_file, index=False, quoting=csv.QUOTE_ALL)
+
+    # # Read classification data, if it exists
+    # csv_file = Path(data_dir + "/external/" + consultation + ".csv")
+    # if csv_file.exists():
+    #     logging.info(f"Chargement des classifications {csv_file}")
+    #     classif = pd.read_csv(csv_file, header=0, quoting=csv.QUOTE_ALL, nrows=1000000)
+    #     logging.info(f"Lu {len(classif)} données de classification")
+    #     classif = classif.drop(columns=["raw_text"])
+    #     responses = responses.merge(classif, on="uid")
+    # else:
+    #     logging.info(f"Pas de classification trouvée dans {csv_file}")
+    #     classif = None
 
 
 
